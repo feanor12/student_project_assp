a0dcd8d69937e97411dbe73f0c12cc75624d832a
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2018-01-18 10:28:33 +0100

    update readme

--- README.m
+++ README.m
@@ -1,1 +1,9 @@
+# Student project for the course Advanced Solid State Physics at Graz
+# University of Technology
+
+The project calculates the density of states for silicon close to the
+valanceband edge.
+
+To run the project using binder use:
+
 [![Binder](https://mybinder.org/badge.svg)](https://mybinder.org/v2/gh/feanor12/student_project_assp/master?filepath=DOS%20Si.ipynb)
